7873116e8f76eda7ca5c175ab296546e921c637a
ToD <wlunareve@gmail.com>
2019-10-31 10:58:19 +0800

    根據需求修改程式

--- cvs_crawler.py
+++ cvs_crawler.py
@@ -18,6 +18,16 @@ def get_web_page(url): #原始地址
     #else:
     return pq(response.text)
 
+def judge_cvs_topic(article_info):
+    topic = ''
+    if  article_info[1][1:3] in ['資訊', '情報', '新聞']:
+        topic = 1
+    elif article_info[1][1:3] in ['商品']:
+        topic = 2
+    elif article_info[1][1:3] in ['問題', '贈送', '討論', '心得']:
+        topic = 3
+    return topic
+
 if __name__ == '__main__':
     PTT_URL = 'https://www.ptt.cc/'
     CSV_URL = 'https://www.ptt.cc/bbs/CVS/index.html'
@@ -25,41 +35,51 @@ if __name__ == '__main__':
     
     output_list = []
     
-    for crawled_page_count in range(1):
+    for crawled_page_count in range(3):
         previous_page_url = current_page_pq('.btn-group-paging .wide')[1].items()[1][1]
         for article in current_page_pq('.r-ent .title a').items():
             article_pq =  get_web_page(f"{PTT_URL}{article.attr('href')}")
             article_info_generator = article_pq('.article-metaline  .article-meta-value').items()
             article_info = [article_info.text() for article_info in article_info_generator]
             
+            if len(article_info) == 0:
+                continue
+            
+            topic = judge_cvs_topic(article_info)
+            
             push_count = 0
             bull_count = 0
             arrow_count = 0
+            push_comment_list = []
+            bull_comment_list = []
+            arrow_comment_list = []
             for push_content in article_pq('.push').items():
-                # print(push_content('.push-content').text()[2:])
+                comment = push_content('.push-content').text()[2:]
                 push_label = push_content('.push-tag').text()
                 if push_label == '推':
                     push_count += 1
+                    push_comment_list.append(comment)
                 elif push_label == '噓':
                     bull_count += 1
+                    bull_comment_list.append(comment)
                 elif push_label == '→':
                     arrow_count += 1
+                    arrow_comment_list.append(comment)
                     
-            try:
-                output_list.append({                
-                    "文章標題": article_info[1],
-                    "文章作者": article_info[0],
-                    "文章時間": covert_datetime_to_string(covert_string_to_datetime(article_info[2], '%a %b %d %H:%M:%S %Y'), '%Y/%m/%d %H:%M:%S'),
-                    "文章網址": f"{PTT_URL}{article.attr('href')}",
-                    "留言數": push_count + bull_count + arrow_count,
-                    "推文數": push_count,
-                    "噓文數": bull_count,
-                    "箭頭數": arrow_count
-                })
-                
-            except:
-                pass
-
+            print(f"{PTT_URL}{article.attr('href')}")
+            output_list.append({                
+                "文章標題": article_info[1],
+                "議題": topic,
+                "文章時間": covert_datetime_to_string(covert_string_to_datetime(article_info[2], '%a %b %d %H:%M:%S %Y'), '%m月%d日'),
+                "文章網址": f"{PTT_URL}{article.attr('href')}",
+                "留言數": push_count + bull_count + arrow_count,
+                "推文數": push_count,
+                "推文留言": push_comment_list,
+                "噓文數": bull_count,
+                "噓文留言": bull_comment_list,
+                "箭頭數": arrow_count,
+                "箭頭留言": arrow_comment_list,
+            })
         current_page_pq = get_web_page(f"{PTT_URL}{previous_page_url}")
         
     with open ('./cvs.json', 'w', encoding='utf-8') as f:
